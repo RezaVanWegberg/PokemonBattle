ba30c6d47560ab23e60c095090d858547c94c648
Reza <99068159@mydavinci.nl>
2023-05-15 10:27:27 +0200

    Heb toegevoegd dat er gevraagd wordt of ze willen starten met de game. En heb een constructor toegevoegd.

--- PokemonBattle1/Program.cs
+++ PokemonBattle1/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System.Runtime.InteropServices;
 
+bool GameStart = true;
+while(GameStart)
+{
+    Console.WriteLine("Start game? answer with Y/N");
+    string VraagGameStart = Console.ReadLine();
+    if (VraagGameStart == "Y")
+    {
+        GameStart = false;
+    }
+}
+
+Console.WriteLine("Enter a name for charmander");
+        string userInput = Console.ReadLine();
+        Charmander myObj = new Charmander(userInput);
+        Console.WriteLine("His name is " + myObj.name);
+
 class Charmander
 {
     public string name;
     public string type;
     public string weakness;
 
-    public Charmander()
+    public Charmander(string input)
     {
-        name = Console.ReadLine();
+        name = input;
         type = "Fire";
         weakness = "Water";
-    }
-    static void Main(string[] args)
-    {
-        Charmander myObj = new Charmander();
-        Console.WriteLine("His name is " + myObj.name);
     }
 }
