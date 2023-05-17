cf22063042c80b7310ff4bcbd92238aa93cbfd73
Reza <99068159@mydavinci.nl>
2023-05-17 13:34:12 +0200

    Heb een begin gemaakt aan de pokeball. Op de juiste manier kan er een pokemon in de pokebal gezet worden

--- PokemonBattle1/Program.cs
+++ PokemonBattle1/Program.cs
@@ -18,6 +18,8 @@ while (StartOver)
     Console.WriteLine("Enter a name for charmander");
     string userInput = Console.ReadLine();
     Charmander myCharmander = new Charmander(userInput, "Fire", "Water");
+    
+    Pokeball greatBall = new Pokeball(myCharmander.name);
 
     for (int i = 0; i < 10; i++)
     {
@@ -30,12 +32,30 @@ while (StartOver)
     if (VraagStartOver == "Y")
     {
         StartOver = true;
+
     }
     else if (VraagStartOver == "N")
     {
         StartOver = false;
+        Console.WriteLine("test");
+        Console.WriteLine(greatBall.CharmanderInPokeball.name);
+
     }
 }
+
+class Pokeball
+{
+    public Pokeball(string pokemon)
+    {
+        List<string> pokeballList = new List<string>(1);
+        pokeballList.Add(pokemon);
+        Console.WriteLine("check dat ie er in zit" + pokeballList[0]); //test om te kijken of de pokemon in de pokebal zit
+
+    }
+
+    public Charmander CharmanderInPokeball { get; set; }
+}
+
 
 class Charmander
 {
@@ -56,5 +76,4 @@ class Charmander
         Console.WriteLine(name + "!");
     }
 }
-
 
